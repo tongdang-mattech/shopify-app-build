93770add9481a7ce237ebb1fbaad9cebbcc6e98d
Tong Mattech <tongdang.mattech@gmail.com>
2023-11-08 16:44:43 +0700

    init

---
+++ assets/None-500b18f1.js
@@ -0,0 +1,1 @@
+import{_ as o,o as n,c as r,m as t}from"./index-8c38fc50.js";const c={};function s(e,a){return n(),r("div",null,[t(e.$slots,"default")])}const _=o(c,[["render",s]]);export{_ as default};
